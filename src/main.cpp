8a0941cef4e3ae1cb388ad022b2b1a3736221a4b
BlakeWerk-eng <jbw0082@auburn.edu>
2025-10-02 15:57:22 -0500

    Removing texture setup because it is not needed for Raymarching

--- src/main.cpp
+++ src/main.cpp
@@ -58,20 +58,20 @@ int main() {
 	unsigned int texture;
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
-	// load and generate the texture
-	int width, height, nrChannels;
-	unsigned char *data = stbi_load("/home/blake/projects/GLFW_test/textures/blue.png", &width, &height,
-			&nrChannels, 0);
-	if (data)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-				GL_UNSIGNED_BYTE, data);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-	{
-		std::cout << "Failed to load texture" << std::endl;
-	}
+////////// load and generate the texture
+////////int width, height, nrChannels;
+////////unsigned char *data = stbi_load("textures/blue.png", &width, &height,
+////////		&nrChannels, 0);
+////////if (data)
+////////{
+////////	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+////////			GL_UNSIGNED_BYTE, data);
+////////	glGenerateMipmap(GL_TEXTURE_2D);
+////////}
+////////else
+////////{
+////////	std::cout << "Failed to load texture" << std::endl;
+////////}
 
 	glActiveTexture(GL_TEXTURE0);
 	glBindTexture(GL_TEXTURE_2D, texture);
